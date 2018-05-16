e30b507226d5af2f9cf3a17003d522441cd8ad76
hoang nguyen <hnguyen4187@gmail.com>
2018-05-16 10:57:12 -0400

    add more fields to user sign up closes #5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :fname, :lname, :username, :avatar])
   end
 end
